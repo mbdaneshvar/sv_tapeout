4887bc388067c6cd9d3e14fb2d79f8c961a28fae
Mo Daneshvar <mohammad.bdaneshvar@lubis-eda.com>
2025-12-16 16:05:35 +0100

    update the top module name

--- src/project.v
+++ src/project.v
@@ -5,7 +5,7 @@
 
 `default_nettype none
 
-module mo_module(
+module tt_um_mo_module(
   input  wire [7:0] ui_in,    // Dedicated inputs
   output wire [7:0] uo_out,   // Dedicated outputs
   input  wire [7:0] uio_in,   // IOs: Input path
